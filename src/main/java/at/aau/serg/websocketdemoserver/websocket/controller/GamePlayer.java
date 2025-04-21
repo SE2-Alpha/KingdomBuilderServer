3eafde7f5a32c6d12590ec2a109fd19192d1ab73
noemi-presser <noemihannah.presser@gmail.com>
2025-04-21 19:03:20 +0200

    fundamental implementation of GameState

--- src/main/java/at/aau/serg/websocketdemoserver/websocket/controller/GamePlayer.java
+++ src/main/java/at/aau/serg/websocketdemoserver/websocket/controller/GamePlayer.java
@@ -1,4 +1,10 @@
 package at.aau.serg.websocketdemoserver.websocket.controller;
 
 public class GamePlayer {
+
+    private String id;
+
+    public GamePlayer(String id) {
+        this.id = id;
+    }
 }
--- src/main/java/at/aau/serg/websocketdemoserver/websocket/state/GameState.java
+++ src/main/java/at/aau/serg/websocketdemoserver/websocket/state/GameState.java
@@ -1,4 +1,23 @@
 package at.aau.serg.websocketdemoserver.websocket.state;
 
+import at.aau.serg.websocketdemoserver.websocket.controller.GamePlayer;
+import at.aau.serg.websocketdemoserver.websocket.controller.GameTurnManager;
+import lombok.Getter;
+
+import java.util.List;
+
 public class GameState {
+
+    @Getter
+    private String gameId;
+    @Getter
+    private GameTurnManager turnManager;
+    @Getter
+    private List<GamePlayer> players;
+
+    public GameState(String gameId, List<String> playerIds) {
+        this.gameId = gameId;
+        this.turnManager = new GameTurnManager(playerIds);
+        this.players = playerIds.stream().map(GamePlayer::new).toList();
+    }
 }
