96786f5dd1fbf991dad2bb6f88a33e18ed43158e
leona <leonardotocci6@gmail.com>
2025-05-11 23:56:38 +0200

    added additional attributes to Player class

--- src/main/java/at/aau/serg/kingdombuilderserver/game/Player.java
+++ src/main/java/at/aau/serg/kingdombuilderserver/game/Player.java
@@ -2,6 +2,10 @@ package at.aau.serg.kingdombuilderserver.game;
 
 public class Player {
     private String id;
+    private String name;
+    private int color;
+    private int remainingSettlements;
+    private int score;
     public Player(String creatorId) {
         this.id = creatorId;
     }
