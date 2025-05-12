176703251d66d9be7394ac5e2def45535c0a91e3
Leonardo Tocci <leonardotocci6@gmail.com>
2025-05-12 23:02:58 +0200

    Implemented some getter/Setter functions for Player class

--- src/main/java/at/aau/serg/kingdombuilderserver/game/Player.java
+++ src/main/java/at/aau/serg/kingdombuilderserver/game/Player.java
@@ -1,16 +1,53 @@
 package at.aau.serg.kingdombuilderserver.game;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan
+@Getter @Setter
 public class Player {
-    private String id;
+    private @Setter(AccessLevel.NONE) String id;
     private String name;
     private int color;
     private int remainingSettlements;
     private int score;
-    public Player(String creatorId) {
-        this.id = creatorId;
+    public Player(String playerId) {
+        this.id = playerId;
+        remainingSettlements = 40;
     }
 
-    public Object getId() {
-        return id;
+    public Player(String playerId, int initialSettlements){
+        this.id = playerId;
+        remainingSettlements = initialSettlements;
     }
+
+    public void setRemainingSettlements(int value) {
+        if(value < 0){
+            remainingSettlements = 0;
+        }else{
+            remainingSettlements = value;
+        }
+    }
+
+    /**
+     *Strictly increases settlement count by the absolute value of the parameter.
+     */
+    public void increaseSettlementsBy(int value){
+        remainingSettlements += Math.abs(value);
+    }
+
+    /**
+     *Strictly decreases settlement count by the absolute value of the parameter.
+     */
+    public void decreaseSettlementsBy(int value){
+        if(remainingSettlements - Math.abs(value) < 0){
+            remainingSettlements = 0;
+        }else{
+            remainingSettlements -= Math.abs(value);
+        }
+    }
+
+
 }
