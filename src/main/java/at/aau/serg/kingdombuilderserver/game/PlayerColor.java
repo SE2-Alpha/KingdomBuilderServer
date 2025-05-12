0f096a58151623a5ef105dab0c10d6bd964c6ad5
Leonardo Tocci <leonardotocci6@gmail.com>
2025-05-13 00:45:22 +0200

    declared colors in PlayerColor as final

--- src/main/java/at/aau/serg/kingdombuilderserver/game/PlayerColor.java
+++ src/main/java/at/aau/serg/kingdombuilderserver/game/PlayerColor.java
@@ -3,8 +3,8 @@ package at.aau.serg.kingdombuilderserver.game;
 import java.awt.*;
 
 public final class PlayerColor {
-    public static int RED = new Color(255, 0, 0).darker().getRGB();
-    public static int BLUE = new Color(132, 43, 226).darker().getRGB();
-    public static int WHITE = new Color(255, 255, 255).darker().getRGB();
-    public static int BLACK = new Color(0, 0, 0).darker().getRGB();
+    public static final int RED = new Color(255, 0, 0).darker().getRGB();
+    public static final int BLUE = new Color(132, 43, 226).darker().getRGB();
+    public static final int WHITE = new Color(255, 255, 255).darker().getRGB();
+    public static final int BLACK = new Color(0, 0, 0).darker().getRGB();
 }
