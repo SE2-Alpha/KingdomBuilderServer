683c686b4ba0e21a332e7921bc9d0726bf26eafe
Leonardo Tocci <leonardotocci6@gmail.com>
2025-05-13 00:44:32 +0200

    Created PlayerColor class

---
+++ src/main/java/at/aau/serg/kingdombuilderserver/game/PlayerColor.java
@@ -0,0 +1,10 @@
+package at.aau.serg.kingdombuilderserver.game;
+
+import java.awt.*;
+
+public final class PlayerColor {
+    public static int RED = new Color(255, 0, 0).darker().getRGB();
+    public static int BLUE = new Color(132, 43, 226).darker().getRGB();
+    public static int WHITE = new Color(255, 255, 255).darker().getRGB();
+    public static int BLACK = new Color(0, 0, 0).darker().getRGB();
+}
