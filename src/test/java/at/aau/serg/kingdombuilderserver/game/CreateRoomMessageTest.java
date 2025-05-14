c993ed0e6e8160c1601c20ca8028e77fd21993d9
noemi-presser <noemihannah.presser@gmail.com>
2025-05-15 00:13:06 +0200

    removed unnecessary import

--- src/test/java/at/aau/serg/kingdombuilderserver/game/CreateRoomMessageTest.java
+++ src/test/java/at/aau/serg/kingdombuilderserver/game/CreateRoomMessageTest.java
@@ -3,7 +3,6 @@ package at.aau.serg.kingdombuilderserver.game;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CreateRoomMessageTest {
 
