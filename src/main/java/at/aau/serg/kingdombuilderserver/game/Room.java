c269914d8e3e7b0a3df256fa9297948ce2e25536
Roman Lipicer <rolipicer@edu.aau.at>
2025-05-28 19:44:02 +0200

    Added RoomLobbyDTO that not the full Room is transfered when the lobby is loaded

--- src/main/java/at/aau/serg/kingdombuilderserver/game/Room.java
+++ src/main/java/at/aau/serg/kingdombuilderserver/game/Room.java
@@ -16,12 +16,25 @@ public class Room {
     private final List<Player> players = new CopyOnWriteArrayList<>();
     @Setter
     private RoomStatus status = RoomStatus.WAITING;
+    private GameManager gameManager;
 
     private static final Logger logger = LoggerFactory.getLogger(Room.class);
 
     public Room(String id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public void startGame() {
+        if (players.size() < 1) { //TODO increase to 2 after debugging
+            logger.warn("Not enough players to start the game. Minimum 2 players required.");
+            return;
+        }
+        setPlayerColor();
+        this.status = RoomStatus.STARTED;
+        this.gameManager = new GameManager();
+        this.gameManager.setActivePlayer(players.get(0)); // Set the first player as the active player
+        logger.info("Game started in room {}", id);
     }
 
     public int getCurrentUsers() {
