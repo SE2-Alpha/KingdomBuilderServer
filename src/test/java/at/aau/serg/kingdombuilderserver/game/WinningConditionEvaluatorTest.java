c3c3dc71db4a54ab2f9368eabafd5a49caee99e7
noemi-presser <noemihannah.presser@gmail.com>
2025-05-29 22:08:59 +0200

    adapted testfile setup + implemented test for testEvaluateDiscoverers

--- src/test/java/at/aau/serg/kingdombuilderserver/game/WinningConditionEvaluatorTest.java
+++ src/test/java/at/aau/serg/kingdombuilderserver/game/WinningConditionEvaluatorTest.java
@@ -2,7 +2,10 @@ package at.aau.serg.kingdombuilderserver.game;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.List;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class WinningConditionEvaluatorTest {
 
@@ -17,15 +20,25 @@ public class WinningConditionEvaluatorTest {
     @BeforeEach
     void setUp() {
         board = new GameBoardWinningConditions();
-        players.add(player1);
-        players.add(player2);
-        players.add(player3);
-        players.add(player4);
+        player1 = new Player("1", "Player1");
+        player2 = new Player("2", "Player2");
+        player3 = new Player("3", "Player3");
+        player4 = new Player("4", "Player4");
+
+        players = new ArrayList<>(Arrays.asList(player1, player2, player3, player4));
+
+        player1.setHouseFieldIds(Set.of(0, 1, 2, 3, 4, 5, 20, 40, 100));
+        player2.setHouseFieldIds(Set.of(7, 9, 28, 46, 65, 90, 103, 141, 199, 203));
+        player3.setHouseFieldIds(Set.of(14, 36, 48, 74, 91, 107, 122, 177, 205, 221));
+        player4.setHouseFieldIds(Set.of(6, 29, 35, 60, 119, 130, 162, 188, 240, 301));
+
         evaluator = new WinningConditionEvaluator(board, players);
     }
 
     @Test
-    public void testConstructor() {
-
+    void testEvaluateDiscoverers() {
+        int points = evaluator.evaluateDiscoverers(player1);
+        assertEquals(4, points);
     }
+    
 }
