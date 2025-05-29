4fb12837f773c826fe4f04c159f76b22335edf4a
noemi-presser <noemihannah.presser@gmail.com>
2025-05-29 19:54:43 +0200

    created structure in class WinningConditionEvaluator

--- src/main/java/at/aau/serg/kingdombuilderserver/game/WinningConditionEvaluator.java
+++ src/main/java/at/aau/serg/kingdombuilderserver/game/WinningConditionEvaluator.java
@@ -1,14 +1,45 @@
 package at.aau.serg.kingdombuilderserver.game;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class WinningConditionEvaluator {
    //Platzhalterklasse soll durch echtes Gameboard ersetzt werden
     private GameBoardWinningConditions board;
-    private List<Player> players;
+    private final List<Player> players;
+    Map<Player, Integer> playerPoints = new HashMap<>();
 
     public WinningConditionEvaluator(GameBoardWinningConditions board, List<Player> players) {
         this.board = board;
         this.players = players;
     }
+
+    public Player evaluateWinner() {
+
+        for (Player player : players) {
+
+        }
+
+        return players.get(0);
+    }
+
+    //Einsiedler
+    public int evaluateHermits(Player player) {
+
+        return 0;
+    }
+
+    //Bergleute
+    public int evaluateMiners(Player player) {
+
+        return 0;
+    }
+
+    //Entdecker
+    public int evaluateDiscoverers(Player player){
+
+        return 0;
+    }
+
 }
