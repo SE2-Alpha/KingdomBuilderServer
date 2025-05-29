42ffce566587d614cd55d63ad07c7289612769c4
noemi-presser <noemihannah.presser@gmail.com>
2025-05-29 20:50:27 +0200

    added javadocs for better comprehension of the method's functionality

--- src/main/java/at/aau/serg/kingdombuilderserver/game/WinningConditionEvaluator.java
+++ src/main/java/at/aau/serg/kingdombuilderserver/game/WinningConditionEvaluator.java
@@ -13,28 +13,55 @@ public class WinningConditionEvaluator {
         this.players = players;
     }
 
+    /**
+     * Geht in einer for-Schleife alle Spieler durch und berechnet für jeden die Gesamtpunktezahl.
+     *
+     * @return winner
+     */
     public Player evaluateWinner() {
 
         for (Player player : players) {
-
+            int points =
+                      evaluateHermits(player)
+                    + evaluateMiners(player)
+                    + evaluateDiscoverers(player);
+            playerPoints.put(player, points);
         }
 
-        return players.get(0);
+        return playerPoints.entrySet().stream()
+                .max(Comparator.comparingInt(Map.Entry::getValue))
+                .map(Map.Entry::getKey)
+                .orElse(null);
     }
 
-    //Einsiedler
+    /**
+     * Berechnet die Punkte, die ein Spieler durch die Karte Einsiedler bekommt.
+     * "1 Gold für jede einzelnstehende eigene Siedlung sowie für jedes separate Siedlungsgebiet."
+     * @param player
+     * @return points from hermits
+     */
     public int evaluateHermits(Player player) {
 
         return 0;
     }
 
-    //Bergleute
+    /**
+     * Berechnet die Punkte, die ein Spieler durch die Karte Bergleute bekommt.
+     * "1 Gold für jede eigene Siedlung, die angrenzend an ein oder mehrere Bergfelder gebaut ist."
+     * @param player
+     * @return points from miners
+     */
     public int evaluateMiners(Player player) {
 
         return 0;
     }
 
-    //Entdecker
+    /**
+     * Berechnet die Punkte, die ein Spieler durch die Karte Entdecker bekommt.
+     * "1 Gold für jede horizontale Linie, auf der mindestens eine eigene Siedlung gebaut ist."
+     * @param player
+     * @return points from Discoverer
+     */
     public int evaluateDiscoverers(Player player){
         Set<Integer> rowsWithSettlements = new HashSet<>();
 
