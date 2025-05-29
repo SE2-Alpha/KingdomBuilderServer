63cb243ce2cdb6181b1c54ceb391f508ecbaa7ca
noemi-presser <noemihannah.presser@gmail.com>
2025-05-29 19:56:32 +0200

    created test class for WinningConditionEvaluator

---
+++ src/test/java/at/aau/serg/kingdombuilderserver/game/WinningConditionEvaluatorTest.java
@@ -0,0 +1,11 @@
+package at.aau.serg.kingdombuilderserver.game;
+
+import org.junit.jupiter.api.Test;
+
+public class WinningConditionEvaluatorTest {
+
+    @Test
+    public void testConstructor() {
+
+    }
+}
