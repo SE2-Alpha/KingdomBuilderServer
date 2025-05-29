d26f6a2c6ef9dc6178a5113fcf5c32ae359d5388
noemi-presser <noemihannah.presser@gmail.com>
2025-05-29 21:02:56 +0200

    implemented variables + setUp method for tests

--- src/test/java/at/aau/serg/kingdombuilderserver/game/WinningConditionEvaluatorTest.java
+++ src/test/java/at/aau/serg/kingdombuilderserver/game/WinningConditionEvaluatorTest.java
@@ -1,8 +1,28 @@
 package at.aau.serg.kingdombuilderserver.game;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 
 public class WinningConditionEvaluatorTest {
+
+    private WinningConditionEvaluator evaluator;
+    private Player player1;
+    private Player player2;
+    private Player player3;
+    private Player player4;
+    private List<Player> players;
+    GameBoardWinningConditions board;
+
+    @BeforeEach
+    void setUp() {
+        board = new GameBoardWinningConditions();
+        players.add(player1);
+        players.add(player2);
+        players.add(player3);
+        players.add(player4);
+        evaluator = new WinningConditionEvaluator(board, players);
+    }
 
     @Test
     public void testConstructor() {
